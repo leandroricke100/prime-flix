207a9b0fe888bda39ecd5523738e4f2834c6ba30
leandroricke100 <leandro_henrique_100@hotmail.com>
2023-09-09 10:10:25 -0300

    update

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,24 @@
+import { useState, useEffect } from "react";
+import api from "../../services/api";
+
 function Home() {
+  const [filmes, setFilmes] = useState([]);
+
+  useEffect(() => {
+    async function loadFilmes() {
+      const response = await api.get("movie/popular", {
+        params: {
+          api_key: "6928a233caa496ec7526dfc6ea0b3f71",
+          language: "pt-BR",
+          page: 1,
+        },
+      });
+      console.log(response.data.results);
+    }
+
+    loadFilmes();
+  }, []);
+
   return (
     <div>
       <h1>Bem vindo a home</h1>
