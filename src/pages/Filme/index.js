1a5c57306c3700c1a90139689e4b0344373e50b2
leandroricke100 <leandro_henrique_100@hotmail.com>
2023-09-09 13:22:03 -0300

    criando pagina de detalhes

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,7 +1,78 @@
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import api from "../../services/api";
+import "./filme-info.css";
+
 function Filme() {
+  const { id } = useParams();
+  const [filme, setFilme] = useState({});
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${id}`, {
+          params: {
+            api_key: "6928a233caa496ec7526dfc6ea0b3f71",
+            language: "pt-BR",
+          },
+        })
+        .then((response) => {
+          //console.log(response.data);
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+          console.log("Não foi possível encontrar o Filme");
+        });
+    }
+
+    loadFilme();
+
+    return () => {
+      console.log("componente foi desmontado");
+    };
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    );
+  }
+
+  function formatarData(data) {
+    const partesData = data.split("-");
+    const ano = partesData[0];
+    const mes = partesData[1];
+    const dia = partesData[2];
+    return `${dia}-${mes}-${ano}`;
+  }
+
   return (
-    <div>
-      <h1>Bem vindo a pagina detalhes de filme</h1>
+    <div className="filme-info">
+      <h1>{filme.title}</h1>
+      <img
+        src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`}
+        alt={filme.title}
+      />
+      <h3>Sinopse</h3>
+      <i>
+        <strong>Lançamento:</strong> {formatarData(filme.release_date)}
+      </i>
+
+      <span>{filme.overview}</span>
+      <strong>
+        Avaliação: {parseFloat(filme.vote_average).toFixed(1)} /10
+      </strong>
+
+      <div className="area-buttons">
+        <button>Salvar</button>
+        <button>
+          <a href="#">Trailer</a>
+        </button>
+      </div>
     </div>
   );
 }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,8 +1,11 @@
 import { useState, useEffect } from "react";
 import api from "../../services/api";
+import { Link } from "react-router-dom";
+import "./home.css";
 
 function Home() {
   const [filmes, setFilmes] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     async function loadFilmes() {
@@ -13,15 +16,38 @@ function Home() {
           page: 1,
         },
       });
-      console.log(response.data.results);
+      //console.log(response.data.results.slice(0, 10));
+      setFilmes(response.data.results.slice(0, 10));
+      setLoading(false);
     }
 
     loadFilmes();
   }, []);
 
+  if (loading) {
+    return (
+      <div className="loading">
+        <h2>Carregando filmes...</h2>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Bem vindo a home</h1>
+    <div className="container">
+      <div className="lista-filmes">
+        {filmes.map((filme) => {
+          return (
+            <article key={filme.id}>
+              <strong>{filme.title}</strong>
+              <img
+                src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`}
+                alt={filme.title}
+              />
+              <Link to={`/filme/${filme.id}`}>Acessar</Link>
+            </article>
+          );
+        })}
+      </div>
     </div>
   );
 }
